e87545e2616a9c726263c03e5c685fe782d6fd03
patrik-drean <patrikdrean@gmail.com>
2018-01-29 07:47:23 -0700

    updating models

--- fomo/settings.py
+++ fomo/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_mako_plus',
-    'homepage',   
+    'homepage',
+    'account',  
 ]
 
 MIDDLEWARE = [
