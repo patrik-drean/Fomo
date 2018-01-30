c4faf6383e4bf5aa2906384ddc346ccc46351777
patrik-drean <patrikdrean@gmail.com>
2018-01-30 08:29:35 -0700

    updated models.py and started test.py

--- account/models.py
+++ account/models.py
@@ -3,8 +3,8 @@ from cuser.models import AbstractCUser
 # from django.contrib.auth.models import AbstractUser
 
 class User(AbstractCUser):
-    first_name = models.TextField(null=True, blank = True)
-    last_name = models.TextField(null=True, blank = True)
+    # first_name = models.TextField(null=True, blank = True)
+    # last_name = models.TextField(null=True, blank = True)
     birthdate = models.DateTimeField(null=True, blank = True)
     address = models.TextField(null=True, blank=True)
     state = models.TextField(null=True, blank=True)
--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+class Tester(TestCase):
+
+    def test_(self):
+        self.assertIn
+        self.assertEquals(a, 50)
+        self.assertTrue(a == 50)
+            
