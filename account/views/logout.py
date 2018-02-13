98f83075c6e33366c0fcb8d3c975094525a328a8
patrik-drean <patrikdrean@gmail.com>
2018-02-12 20:02:53 -0700

    Updated for postgresql

---
+++ account/views/logout.py
@@ -0,0 +1,9 @@
+from django_mako_plus import view_function, jscontext
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+
+@view_function
+def process_request(request):
+        context = {}
+        logout(request)
+        return HttpResponseRedirect('/homepage/index/')
--- fomo/settings.py
+++ fomo/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'homepage',
     'account',
     'formlib',
+    'catalog',
+
 ]
 
 # AUTH_USER_MODEL = 'cuser.CUser' # This is the github user
@@ -151,10 +153,15 @@ WSGI_APPLICATION = 'fomo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fomo_db',
+        'USER': 'patrikdrean',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
