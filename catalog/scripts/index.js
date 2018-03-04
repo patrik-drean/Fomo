fadbe05c6b949aa96223b6bcf41bc3c520490cc6
patrik-drean <patrikdrean@gmail.com>
2018-03-03 22:20:40 -0700

    updated ajax on catalog

--- catalog/scripts/index.js
+++ catalog/scripts/index.js
@@ -1,7 +1,8 @@
 $(function(context) {
    return function() {
-      var container = $('#catalog_container')
-      container.load('/catalog/index.products')
+      var catalog = $('#catalog')
+      catalog.load('/catalog/index.products/')
+      console.log(context)
 
 }
 }(DMP_CONTEXT.get()))
--- catalog/views/index.py
+++ catalog/views/index.py
@@ -12,12 +12,25 @@ def process_request(request, categoryid = 0):
         category = None
         products = cmod.Product.objects.all()
     categories = cmod.Category.objects.all()
+    productCount = int(len(products) / 6) + 1
     context = {'products': products,
                 'categories': categories,
-                'category': category,}
+                'category': category,
+                'productCount': productCount}
     return request.dmp.render('index.html', context)
 
 @view_function
-def products(request):
-    context = {}
+def products(request, categoryid = 0):
+    if int(categoryid) > 0:
+        category = cmod.Category.objects.get(id = categoryid)
+        products = cmod.Product.objects.all().filter(Category_id = category.id, Status = 'A')
+    else:
+        category = None
+        products = cmod.Product.objects.all()
+    categories = cmod.Category.objects.all()
+    productCount = int(len(products) / 6) + 1
+    context = {'products': products,
+                'categories': categories,
+                'category': category,
+                'productCount': productCount}
     return request.dmp.render('index.products.html', context)
