f72ec3aaf92233948caabb6504786a33e78a81ea
patrik-drean <patrikdrean@gmail.com>
2018-03-05 20:39:38 -0700

    finished the index.products html functionality, including css and js

--- catalog/scripts/index.js
+++ catalog/scripts/index.js
@@ -1,8 +1,34 @@
+var categoryid
+var pageid
+var totalPageCount
+
 $(function(context) {
    return function() {
+      // Load the 'catalog' div with 6 product tiles
       var catalog = $('#catalog')
-      catalog.load('/catalog/index.products/')
-      console.log(context)
+      categoryid = context.categoryid
+      pageid = 1
+      totalPageCount = context.totalPageCount
+      catalog.load('/catalog/index.products/' + categoryid + '/' + pageid)
 
 }
-}(DMP_CONTEXT.get()))
+
+}(DMP_CONTEXT.get('catalog/index')))
+
+function changeFunction(pageIncrement = 1) {
+      // Grab the current page number from div and increment
+      var currentPageNumber = parseInt($('#currentPageNumber').text())
+      var pageid = currentPageNumber + pageIncrement
+      
+      // Load the 'catalog' div with 6 product tiles
+      if (pageid > 0 && pageid <= totalPageCount)
+      {
+         var catalog = $('#catalog')
+         catalog.load('/catalog/index.products/' + categoryid + '/' + pageid)
+         console.log(pageid)
+      }
+
+      // <a href="/catalog/index.products/${ category.id }/${ int(pageid) - 1  }"><i class="fas fa-angle-double-left"></i></a>
+      // <p class="">Page ${ int(pageid) + 1 } of ${ productCount }</p>
+      // <a href="/catalog/index.products/${ category.id }/${ int(pageid) + 1  }"><i class="fas fa-angle-double-right"></i></a>
+}
--- catalog/views/index.py
+++ catalog/views/index.py
@@ -7,30 +7,57 @@ from catalog import models as cmod
 def process_request(request, categoryid = 0):
     if int(categoryid) > 0:
         category = cmod.Category.objects.get(id = categoryid)
-        products = cmod.Product.objects.all().filter(Category_id = category.id, Status = 'A')
+        products = cmod.Product.objects.all().filter(Category_id = category.id).filter(Status = 'A')
     else:
         category = None
-        products = cmod.Product.objects.all()
+        products = cmod.Product.objects.all().filter(Status = 'A')
+
+    totalPageCount = int(len(products) / 6) + 1
     categories = cmod.Category.objects.all()
-    productCount = int(len(products) / 6) + 1
-    context = {'products': products,
-                'categories': categories,
+    context = { 'categories': categories,
                 'category': category,
-                'productCount': productCount}
+                jscontext('categoryid'): categoryid,
+                jscontext('pageid'): 0,
+                jscontext('totalPageCount'): totalPageCount,}
+
     return request.dmp.render('index.html', context)
 
 @view_function
-def products(request, categoryid = 0):
+def products(request, categoryid = 0, pageid:int = 1):
+
+    # Load the products based off of category
     if int(categoryid) > 0:
         category = cmod.Category.objects.get(id = categoryid)
-        products = cmod.Product.objects.all().filter(Category_id = category.id, Status = 'A')
+        products = cmod.Product.objects.all().filter(Category_id = category.id).filter(Status = 'A')
     else:
         category = None
-        products = cmod.Product.objects.all()
+        products = cmod.Product.objects.all().filter(Status = 'A')
+
+    # Divide the products up to show total pages and only display 6 products
+    totalPageCount = int(len(products) / 6) + 1
+
+    sixProducts = []
+
+    if pageid == totalPageCount:
+        maxProductPosition = len(products)
+        productPosition = (pageid - 1) * 6
+    else:
+        maxProductPosition = pageid * 6
+        productPosition = (pageid - 1) * 6
+
+    while(productPosition < maxProductPosition):
+        if products[productPosition] is not None:
+            sixProducts.append(products[productPosition])
+        productPosition += 1
+
     categories = cmod.Category.objects.all()
-    productCount = int(len(products) / 6) + 1
-    context = {'products': products,
+    context = {'sixProducts': sixProducts,
                 'categories': categories,
                 'category': category,
-                'productCount': productCount}
+                'totalPageCount': totalPageCount,
+                'pageid': pageid,
+                jscontext('categoryid'): categoryid,
+                jscontext('pageid'): pageid,
+                jscontext('totalPageCount'): totalPageCount,}
+
     return request.dmp.render('index.products.html', context)
