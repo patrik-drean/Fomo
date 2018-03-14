c30715cec57224d5d2e525d5de5bd2c2ae14035d
patrik-drean <patrikdrean@gmail.com>
2018-03-14 09:57:58 -0600

    completed 1st section of sprint 6. Added detail page and moved catalog links to app_base

---
+++ catalog/views/detail.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django_mako_plus import view_function, jscontext
+from datetime import datetime, timezone
+from catalog import models as cmod
+
+@view_function
+def process_request(request):
+
+    context = { 
+                }
+
+    return request.dmp.render('detail.html', context)
