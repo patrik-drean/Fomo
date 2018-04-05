eca8fdb7d952d8d90cd546c8bd7aedfe1b45b0b2
patrik-drean <patrikdrean@gmail.com>
2018-04-05 11:56:12 -0600

    updated email to send from musical-family.me

--- homepage/views/contact.py
+++ homepage/views/contact.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django_mako_plus import view_function, jscontext
 from datetime import datetime, timezone
-# import smtplib
-# from email.mime.multipart import MIMEMultipart
-# from email.MIMEText import MIMEText
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 @view_function
 def process_request(request):
@@ -15,25 +15,26 @@ def process_request(request):
         print(request.POST['inputMessage'])
 
         # Send an actual email
-        # fromaddr = "dreantester@gmail.com"
-        # toaddr = request.POST['inputEmail']
-        # subject = request.POST['inputSubject']
-        # message = request.POST['inputMessage']
-        #
-        # msg = MIMEMultipart()
-        # msg['From'] = fromaddr
-        # msg['To'] = toaddr
-        # msg['Subject'] = subject
-        #
-        # body = message
-        # msg.attach(MIMEText(body, 'plain'))
-        #
-        # server = smtplib.SMTP('smtp.gmail.com', 587)
+        fromaddr = "pdrean@musical-family.me"
+        toaddr = request.POST['inputEmail']
+        subject = request.POST['inputSubject']
+        message = request.POST['inputMessage']
+
+        msg = MIMEMultipart()
+        msg['From'] = fromaddr
+        msg['To'] = toaddr
+        msg['Subject'] = subject
+
+        body = message
+        msg.attach(MIMEText(body, 'html'))
+
+        server = smtplib.SMTP('mail.musical-family.me', 25)
         # server.starttls()
         # server.login(fromaddr, "TestAcount")
-        # text = msg.as_string()
-        # server.sendmail(fromaddr, toaddr, text)
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
         # server.quit()
+
     context = {
     }
     return request.dmp.render('contact.html', context)
