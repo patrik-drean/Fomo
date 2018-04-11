dc7c241f8462a91fbe9336d66e658c8b3c95b2c8
patrik-drean <patrikdrean@gmail.com>
2018-04-11 08:01:56 -0600

    changed active items to a list

--- catalog/models.py
+++ catalog/models.py
@@ -135,7 +135,7 @@ class Order(models.Model):
                 product_id = 75)
 
             #  grab tax item and append to the end
-            tax_item = OrderItem.objects.get(order_id = self.id, product_id = 75)
+            tax_item = list(OrderItem.objects.get(order_id = self.id, product_id = 75))
             items.appened(tax_item)
 
         # if we aren't including the tax item, alter the
